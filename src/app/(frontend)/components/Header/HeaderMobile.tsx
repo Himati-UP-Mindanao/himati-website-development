b76f1c6ebcc583e8cd95c30f31313553f4efe681
-m <villamorjohnnoel@gmail.com>
2025-01-20 12:13:04 +0800

    fix: fix z-index issue on the mobile header

--- src/app/(frontend)/components/Header/HeaderMobile.tsx
+++ src/app/(frontend)/components/Header/HeaderMobile.tsx
@@ -15,6 +15,7 @@ const MobileHeader = () => {
   const [isOpen, setIsOpen] = useState(false)
 
   const toggleMenu = () => {
+    console.log('clicked')
     setIsOpen(!isOpen)
   }
 
@@ -30,31 +31,33 @@ const MobileHeader = () => {
             maskComposite: 'exclude',
           }}
         ></div>
-        <div className="flex flex-row items-center justify-between w-full">
-          <IoMenu className="h-5 w-5 text-xl font-semibold text-white" onClick={toggleMenu} />
-          <div className="flex flex-row items-center border-[1.5px] rounded-full pr-4">
-            <Image src={HimatiIcon} alt="Himati Icon" className="h-11 w-11" />
-            <h1 className="text-lg text-white">HIMATI</h1>
+        <div className='flex flex-row items-center justify-between w-full z-10'> 
+          <div className="flex flex-row items-center justify-between w-full">
+            <IoMenu className="h-5 w-5 text-xl font-semibold text-white" onClick={toggleMenu} />
+            <div className="flex flex-row items-center border-[1.5px] rounded-full pr-4">
+              <Image src={HimatiIcon} alt="Himati Icon" className="h-11 w-11" />
+              <h1 className="text-lg text-white">HIMATI</h1>
+            </div>
+            <FiSearch className="h-5 w-5 text-xl font-semibold text-white" />
           </div>
-          <FiSearch className="h-5 w-5 text-xl font-semibold text-white" />
         </div>
-      </div>
-      <nav
-        className="h-screen w-2/5 fixed top-0 left-0 bg-negative-900 transition-all duration-300 ease-in-out transform translate-x-[-100%]"
-        style={{ transform: isOpen ? 'translateX(0)' : 'translateX(-100%)' }}
-      >
-        <div className="px-8 py-6 flex justify-end w-full">
-          <IoCloseOutline className="h-8 w-8 text-white" onClick={toggleMenu} />
-        </div>
+        <nav
+          className="h-screen w-2/5 fixed top-0 left-0 bg-negative-900 transition-all duration-300 ease-in-out transform translate-x-[-100%] z-20"
+          style={{ transform: isOpen ? 'translateX(0)' : 'translateX(-100%)' }}
+        >
+          <div className="px-8 py-6 flex justify-end w-full">
+            <IoCloseOutline className="h-8 w-8 text-white" onClick={toggleMenu} />
+          </div>
 
-        <ul className="flex flex-col justify-center text-white font-bold">
-          {LINKS.map((link, index) => (
-            <li key={index} className="border-b border-white px-8 py-4">
-              <Link href="/">{link}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
+          <ul className="flex flex-col justify-center text-white font-bold">
+            {LINKS.map((link, index) => (
+              <li key={index} className="border-b border-white px-8 py-4">
+                <Link href="/">{link}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
     </header>
   )
 }
