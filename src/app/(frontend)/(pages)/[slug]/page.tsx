270cd5abc3229ccff4873c57d4859e615d0e70be
-m <villamorjohnnoel@gmail.com>
2025-02-02 03:26:04 +0800

    feat: enable data caching and server-side rendering

--- src/app/(frontend)/(pages)/[slug]/page.tsx
+++ src/app/(frontend)/(pages)/[slug]/page.tsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import HighlightSection from '../../components/HighlightSection'
 import ArticleSection from '../../components/ArticleSection'
-import { notFound } from 'next/navigation'
 import IssueSection from '../../components/IssueSection'
 
-export const revalidate = 60
+export const revalidate = 1800 // 30 minutes
 
-const VALID_SLUGS = ['news', 'features', 'kultura', 'opinion', 'pamati']
+export const dynamicParams = false;
+
+export const generateStaticParams = async () => { 
+  const VALID_SLUGS: string[] = ['news', 'features', 'kultura', 'opinion', 'pamati']
+
+  return VALID_SLUGS.map((slug) => ({
+    slug: String(slug),
+  }))
+}
+
 
 const Page = async ({ params }: { params: Promise<{ slug: string }> }) => {
   const slug = (await params).slug
-
-  if (!VALID_SLUGS.includes(slug)) {
-    return notFound()
-  }
 
   return (
     <main className="px-8 py-2 lg:py-32 max-w-screen-xl mx-auto font-acronym space-y-16">
